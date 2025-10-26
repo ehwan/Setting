99db8cdfb34d9b06b836317d7966299c5fc82364
Taehwan Kim <ehwank98@gmail.com>
2025-10-26 22:52:05 +0900

    fix rustdesk script

--- init_rustdesk.sh
+++ init_rustdesk.sh
@@ -20,6 +20,7 @@ curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 cargo install lsd
 cargo install starship
 
-sudo docker image pull rustdesk/rustdesk-server
-sudo docker run --name hbbs -v ./data:/root -td --net=host --restart unless-stopped rustdesk/rustdesk-server hbbs
-sudo docker run --name hbbr -v ./data:/root -td --net=host --restart unless-stopped rustdesk/rustdesk-server hbbr
+
+bash <(wget -qO- https://get.docker.com)
+wget rustdesk.com/oss.yml -O compose.yml
+sudo docker compose up -d
